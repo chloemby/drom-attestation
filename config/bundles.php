ef8e4d7c535abc31cfbc2f7661914643faef7749
Vladislav Goncharov <goncharov_vs@drom.ru>
2022-04-15 12:09:04 +0700

    Remove doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -3,6 +3,4 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
